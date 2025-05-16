404f6416017e3f23c8f0cad2528d938d4696002d
Max Nest <udomix.udomix@gmail.com>
2025-05-16 14:11:05 +0000

    Add audio recording and refactor

--- web/script.js
+++ web/script.js
@@ -1,31 +1,83 @@
 import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.js';
 
+const IDLE = 0;
+const TRANSCRIBING = 1;
+const PENDING = 2;
+const RECORDING = 3;
+
 createApp({
     data() {
         return {
-            loading: false,
+            state: IDLE,
             transcription: [],
+            recorder: null,
         };
     },
+    computed: {
+        isIdle() {
+            return this.state === IDLE;
+        },
+        isTranscribing() {
+            return this.state === TRANSCRIBING;
+        },
+        isPending() {
+            return this.state === PENDING;
+        },
+        isRecording() {
+            return this.state === RECORDING;
+        },
+    },
     methods: {
-        handleFileChange(event) {
-            const file = event.target.files[0];
-            if (file) this.transcribeFile(file);
-        },
-        handleFileButtonClick() {
-            if (!this.loading) document.querySelector('#audioFileInput').click();
-        },
-        async transcribeFile(file) {
-            this.loading = true;
+        async transcribeAudio(audio) {
+            this.state = TRANSCRIBING;
             this.transcription = [];
             const data = new FormData();
-            data.append('audio', file);
+            data.append('audio', audio);
             const response = await fetch('/api/transcribe', { method: 'POST', body: data });
-            if (!response.ok)
-                throw new Error(`HTTP Error ${response.status}`);
+            if (!response.ok) {
+                console.error(`HTTP Error ${response.status}`);
+                alert('Произошла ошибка сервера');
+                this.state = IDLE;
+                return;
+            }
             const result = await response.json();
             this.transcription = result.transcription;
-            this.loading = false;
+            this.state = IDLE;
+        },
+        handleFileChange(event) {
+            const file = event.target.files[0];
+            if (file) this.transcribeAudio(file);
+        },
+        handleFileButtonClick() {
+            if (this.state !== IDLE) return;
+            document.querySelector('#audioFileInput').click();
+        },
+        handleRecordStartClick() {
+            if (this.state !== IDLE) return;
+            this.state = PENDING;
+            navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+                this.state = RECORDING;
+                const recorder = this.recorder = new MediaRecorder(stream);
+                const chunks = [];
+                recorder.start();
+                recorder.ondataavailable = event => chunks.push(event.data);
+                recorder.onstop = () => {
+                    const blob = new Blob(chunks);
+                    this.transcribeAudio(blob);
+                };
+                recorder.onerror = event => {
+                    console.error(event.error);
+                    alert('Произошла ошибка при записи звука');
+                    this.state = IDLE;
+                };
+            }).catch(error => {
+                console.error(error);
+                alert('Не удалось получить доступ к микрофону');
+                this.state = IDLE;
+            });
+        },
+        handleRecordStopClick() {
+            if (this.recorder) this.recorder.stop();
         }
     },
 }).mount('.container');
